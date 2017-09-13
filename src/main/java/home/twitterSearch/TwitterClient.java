6c677659a243aa5f9ba2e6f532393e537649aace
niesh <niesh@NIESH7.asiapacific.hpqcorp.net>
2017-09-13 08:07:10 +0800

    11

--- src/main/java/home/twitterSearch/TwitterClient.java
+++ src/main/java/home/twitterSearch/TwitterClient.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
@@ -13,13 +15,14 @@ public class TwitterClient {
 	private String tokenString;
 	public TwitterClient(){
 		httpClient = new DefaultHttpClient();
-		HttpGet getRequest = new HttpGet(
+		HttpPost postRequest = new HttpPost(
 				"https://api.twitter.com/oauth2/token");
-		getRequest.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-		getRequest.addHeader("Authorization", "Basic Q0xadjFpVU91M3JYMTFYRlpDam9Db1RFYjpXdmF6eFp5UDhvdzQ4TklhcHBUOGVNcnVnWk9CSURnMXN2cDFLV0hIQWw0REJDQjUwWQ==");
+		postRequest.addHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+		postRequest.addHeader("Authorization", "Basic Q0xadjFpVU91M3JYMTFYRlpDam9Db1RFYjpXdmF6eFp5UDhvdzQ4TklhcHBUOGVNcnVnWk9CSURnMXN2cDFLV0hIQWw0REJDQjUwWQ==");
+		postRequest.setEntity(new ByteArrayEntity("grant_type=client_credentials".getBytes()));
 		HttpResponse tokenResp=null;
 		try {
-			tokenResp= httpClient.execute(getRequest);
+			tokenResp= httpClient.execute(postRequest);
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 			System.exit(1);
